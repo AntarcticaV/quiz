49a4660379ff97916f2d98cdb8931d0e49f92a12
AntarcticaV <beliyroman@outlook.com>
2023-03-04 15:00:28 +0700

    Otvet

--- main.py
+++ main.py
@@ -1,46 +1,41 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 from kivy.uix.label import Label
-from kivy.config import Config
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.gridlayout import GridLayout
 
 
-class QuizApp(App):
+class check_box(GridLayout):
 
+    def __init__(self, **kwargs):
+        super(check_box, self).__init__(**kwargs)
+
+        self.cols = 2
+
+        self.add_widget(Label(text='Male'))
+        self.active = CheckBox(active=True)
+        self.add_widget(self.active)
+
+        self.lbl_active = Label(text='Checkbox is on')
+        self.add_widget(self.lbl_active)
+
+        self.active.bind(active=self.on_checkbox_Active)
+
+    def on_checkbox_Active(self, checkboxInstance, isActive):
+        if isActive:
+            self.lbl_active.text = "Checkbox is ON"
+            print("Checkbox Checked")
+        else:
+            self.lbl_active.text = "Checkbox is OFF"
+            print("Checkbox unchecked")
+
+
+class CheckBoxApp(App):
     def build(self):
-        self.questions = ['What color is the sky?',
-                          'What is 2+2?', 'What is the capital of France?']
-        self.answers = ['blue', '4', 'Paris']
-        self.index = 0
 
-        layout = BoxLayout(orientation='vertical')
-        self.question_label = Label(
-            text=self.questions[self.index], size_hint=(1, 0.5))
-        layout.add_widget(self.question_label)
-
-        self.answer_button = Button(text='Show answer', size_hint=(1, 0.2))
-        self.answer_button.bind(on_press=self.show_answer)
-        layout.add_widget(self.answer_button)
-
-        self.next_button = Button(text='Next question', size_hint=(1, 0.2))
-        self.next_button.bind(on_press=self.next_question)
-        layout.add_widget(self.next_button)
-
-        return layout
-
-    def show_answer(self, instance):
-        self.question_label.text = self.answers[self.index]
-
-    def next_question(self, instance):
-        self.index += 1
-        if self.index == len(self.questions):
-            self.index = 0
-        self.question_label.text = self.questions[self.index]
+        return check_box()
 
 
+# Run the app
 if __name__ == '__main__':
-    Config.set("graphics", "resizable", 0)
-    Config.set("graphics", "width", 360)
-    Config.set("graphics", "height", 800)
-    Config.write()
-    QuizApp().run()
+    CheckBoxApp().run()
